3febc1eaee1a4b70e5405f3bc094ff68e2f3bc9e
LI Ziming <15071186776@163.com>
2024-09-29 10:35:18 +0800

    Update run_multi_agents.sh

--- run_multi_agents.sh
+++ run_multi_agents.sh
@@ -3,7 +3,9 @@
 # Default values
 # "titanic" "spaceship_titanic" 
 competitions=("ghouls_goblins_and_ghosts_boo" "bank_churn" "classification_with_an_academic_success_dataset")
-runs=2
+start_run=1
+end_run=3
+dest_dir_param="tool_data_cleaning"
 
 # Function to run a single experiment
 run_experiment() {
@@ -17,7 +19,7 @@ run_experiment() {
 
     # Define source and destination directories
     source_dir="multi_agents/competition/$competition"
-    dest_dir="multi_agents/experiments_history/$competition/tool_data_cleaning/$run_number"
+    dest_dir="multi_agents/experiments_history/$competition/$dest_dir_param/$run_number"
 
     # Create destination directory if it doesn't exist
     mkdir -p "$dest_dir"
@@ -38,31 +40,10 @@ run_experiment() {
     done
 }
 
-# Parse command line arguments
-while [[ $# -gt 0 ]]; do
-    key="$1"
-    case $key in
-        --competitions)
-        IFS=' ' read -r -a competitions <<< "$2"
-        shift
-        shift
-        ;;
-        --runs)
-        runs="$2"
-        shift
-        shift
-        ;;
-        *)
-        echo "Unknown option: $1"
-        exit 1
-        ;;
-    esac
-done
-
 # Run experiments for each competition
 for competition in "${competitions[@]}"; do
-    for ((run=1; run<=runs; run++)); do
-        run_experiment "$competition" "$run"
+    for ((run=start_run; run<=end_run; run++)); do
+        run_experiment "$competition" "$run" "$dest_dir_param"
     done
 done
 
